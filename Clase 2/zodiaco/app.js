39e45bc7a6cb2e5fe9df3d73e6387f548cf81b6b
cris664 <50972058+cris664@users.noreply.github.com>
2022-08-29 19:21:45 -0300

    Delete Clase 2/zodiaco directory

--- Clase 2/zodiaco/app.js
+++
@@ -1,5 +0,0 @@
-let persona = require("./persona");
-const zodiac = require('zodiac-signs')('en');
-console.log(persona[0]);
-//console.log(persona+zodiac.getSignByDate({day:2,month:2}));
-console.log(zodiac.getSignByDate({day:2,month:2}));
