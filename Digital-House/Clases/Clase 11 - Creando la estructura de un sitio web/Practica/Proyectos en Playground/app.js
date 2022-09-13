5a6947b6f7a82ce2c2e58d5f993580ac86173513
cris664 <cristianliu.1999@gmail.com>
2022-09-12 21:02:29 -0300

    Nuevas COsas

---
+++ Digital-House/Clases/Clase 11 - Creando la estructura de un sitio web/Practica/Proyectos en Playground/app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+//app.use(express.static("public"))
+app.listen(3030,()=>{
+    console.log("Levantando servidor")
+})
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
