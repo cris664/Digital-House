58a9b344e8f67ded1ae9c524e44b949fb5cbcebf
cris664 <cristianliu.1999@gmail.com>
2022-09-12 22:05:46 -0300

    Ej terminado

--- Digital-House/Clases/Clase 11 - Creando la estructura de un sitio web/Practica/Proyectos en Playground/app.js
+++ Digital-House/Clases/Clase 11 - Creando la estructura de un sitio web/Practica/Proyectos en Playground/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-//app.use(express.static("public"))
+app.use(express.static("public"))
 app.listen(3030,()=>{
     console.log("Levantando servidor")
 })
